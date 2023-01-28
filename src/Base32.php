308948e7f200fdb03eecd4ef99f4b48438f362bb
Denis Makula <denis@emdeevy.me>
2023-01-28 20:21:51 +0200

    fix encode method (much more efficient and actually works)

--- src/Base32.php
+++ src/Base32.php
@@ -1,7 +1,5 @@
 <?
 
-declare(strict_types = 1);
-
 namespace emdeevy\base32;
 
 use InvalidArgumentException;
@@ -10,82 +8,56 @@ class Base32
 {
 
     protected const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567=';
+    protected const PADDING_INDEX = 32;
 
     /**
-     * Encodes a string in base32 format
+     * Encodes a given string into base32. Ridiculously commented to keep track with the algorithm.
      *
-     * @param non-empty-string $string The input string to be encoded
-     * @return string The base32 encoded string
-     * @throws InvalidArgumentException if the input string is empty
-     */
-    public static function encode(string $string): string
-    {
-        // Check if input string is empty
-        $string !== '' || throw new InvalidArgumentException('Attempt of empty string encoding');
-
-        // Handle padding
-        $padding_length = (8 - (strlen($string) * 8) % 5) % 8;
-        $string .= str_repeat("\0", $padding_length);
-        // Initialize variables for encoding
-        $encoded = '';
-        $val = 0;
-        $bit_length = 0;
-
-        // Iterate through each character of the input string
-        for ($i = 0; $i < strlen($string); $i++) {
-            // Shift the value to the left by 8 bits
-            $val = ($val << 8) | ord($string[$i]);
-            // Increase the bit length by 8
-            $bit_length += 8;
-            // Continue while there are at least 5 bits
-                while ($bit_length >= 5) {
-                    // Append the encoded character to the encoded string
-                    $encoded .= self::ALPHABET[($val >> ($bit_length - 5)) & 31];
-                    // Decrease the bit length by 5
-                    $bit_length -= 5;
-                }
-        }
-
-        // Remove padding from the encoded string
-        if ($padding_length) {
-            $encoded = substr($encoded, 0, -$padding_length);
-        }
-
-        // Return the encoded string
-        return $encoded;
-    }
-
-    /**
-     * Decodes a base32 encoded string.
+     * @param string $string Text string to be encoded
+     * @param bool $padding Flag whether the output should have padding or not
      *
-     * @param string $encoded The base32 encoded string
-     * @throws InvalidArgumentException if the input string is empty
-     * @return string The decoded string
+     * @return string Base32 encoded string
      */
-    public static function decode(string $encoded): string
+    public static function encode(string $string, bool $padding = false): string
     {
-        // Validate input string is not empty
-        $encoded !== '' || throw new InvalidArgumentException('Attempt of empty string decoding');
+        $string !== '' || throw new InvalidArgumentException(); // we don't encode empty strings
 
-        // Initialize variables for decoding
-        $decoded = '';
-        $val = 0;
-        $bit_length = 0;
+        $output = ''; // initialize encoded output string
+        $index_iterator = 0; // iterator that goes through the array mapping the characters with their decimal equivalent
+        $processing_chunk_size = 0; // the size of the chunk we are currently working with
+        $processing_chunk_bits = 0; // the bits of the chunk we are currently working with
 
-        // Iterate through the encoded string
-        for ($i = 0; $i < strlen($encoded); $i++) {
-            // Shift the value left by 5 bits and add the new character's value
-            $val = ($val << 5) | strpos(self::ALPHABET, $encoded[$i]);
-            $bit_length += 5;
-            // If the bit length is 8 or more, add the decoded character to the decoded string
-            if ($bit_length >= 8) {
-                $decoded .= chr(($val >> ($bit_length - 8)) & 255);
-                $bit_length -= 8;
+        $original_input_length = strlen($string); // length of the original string from the parameter
+        $_input = $string . str_repeat(chr(0), 4); // the original string appended with 4 zeros, to make sure it is at least 40 bits and make it easy to determine when to stop the encoding process
+        $decimals = unpack('C*', $_input); // unpack the string into an array, mapping its characters with their decimal equivalent
+
+        while ($index_iterator < $original_input_length || $processing_chunk_size !== 0) { // while whether there are still characters to be processed or the current processing chunk is not empty, do:
+            if ($processing_chunk_size < 5) { // if the current processing chunk falls under 5 bits, add the bits of the next character to the current processing chunk
+                $processing_chunk_size += 8; // increase the current processing chunk size by 8
+                $processing_chunk_bits <<= 8; // add 8 zeros to the right current processing chunk
+                $processing_chunk_bits += $decimals[++$index_iterator]; // add the next character's bits to the zeros we added
             }
+
+            // Now we need to get the first 5 bits from the processing chunk, turn it into a character from the base32 alphabet and append it to the decoded string
+            $remaining_chunk_size = $processing_chunk_size - 5; // the remaining chunk size after we take the first 5 bits
+            $remaining_chunk_bits = $processing_chunk_bits & ((1 << $remaining_chunk_size) - 1); // using a remaining-bits-size minus 1 amount of ones to the right, we can bitwise 'AND' to get the remaining chunk bits
+
+            $outside_character = ($index_iterator - (int)($processing_chunk_size > 8)) > $original_input_length; // we either prepared the index for the next character or we didn't, if we did, check against index minus one, if we didn't, check against current index, whether the current character is out of the original input's length bounds, true if so, false otherwise
+
+
+            if($outside_character && ($processing_chunk_bits == 0)) { // if current character is out of the original input's length bounds, and the processing chunk is filled with zeroes, we pad
+                $output .= ($padding) ? static::ALPHABET[static::PADDING_INDEX] : ''; // if the flag not to pad is set, well, we don't pad
+            }
+            else {
+                $output .= static::ALPHABET[$processing_chunk_bits >> $remaining_chunk_size]; // this isn't padding, it's just a character, so we get it from the base32 alphabet using the decimal equivalent of the first 5 bits as index
+            }
+
+            $processing_chunk_bits = $remaining_chunk_bits; // update current processing chunk bits to the remaining chunk bits after we used the first 5 bits
+            $processing_chunk_size = $remaining_chunk_size; // update the current processing chunk size according to what it currently is
         }
 
-        // Return the decoded string
-        return $decoded;
+        return $output; // we're done
+
     }
 
 }
--- test/Base32.php
+++ test/Base32.php
@@ -10,19 +10,19 @@ final class Base32 extends TestCase
     public function testEncodeReturnsCorrectEncoding(): void
     {
         $this->assertEquals(
-            'MY======',
+            'MY',
             \emdeevy\base32\Base32::encode('f')
         );
         $this->assertEquals(
-            'MZXQ====',
+            'MZXQ',
             \emdeevy\base32\Base32::encode('fo')
         );
         $this->assertEquals(
-            'MZXW6===',
+            'MZXW6',
             \emdeevy\base32\Base32::encode('foo')
         );
         $this->assertEquals(
-            'MZXW6YQ=',
+            'MZXW6YQ',
             \emdeevy\base32\Base32::encode('foob')
         );
         $this->assertEquals(
@@ -30,7 +30,7 @@ final class Base32 extends TestCase
             \emdeevy\base32\Base32::encode('fooba')
         );
         $this->assertEquals(
-            'MZXW6YTBOI======',
+            'MZXW6YTBOI',
             \emdeevy\base32\Base32::encode('foobar')
         );
     }
@@ -39,19 +39,19 @@ final class Base32 extends TestCase
     {
         $this->assertEquals(
             'f',
-            \emdeevy\base32\Base32::decode('MY======')
+            \emdeevy\base32\Base32::decode('MY')
         );
         $this->assertEquals(
             'fo',
-            \emdeevy\base32\Base32::decode('MZXQ====')
+            \emdeevy\base32\Base32::decode('MZXQ')
         );
         $this->assertEquals(
             'foo',
-            \emdeevy\base32\Base32::decode('MZXW6===')
+            \emdeevy\base32\Base32::decode('MZXW6')
         );
         $this->assertEquals(
             'foob',
-            \emdeevy\base32\Base32::decode('MZXW6YQ=')
+            \emdeevy\base32\Base32::decode('MZXW6YQ')
         );
         $this->assertEquals(
             'fooba',
@@ -59,7 +59,7 @@ final class Base32 extends TestCase
         );
         $this->assertEquals(
             'foobar',
-            \emdeevy\base32\Base32::decode('MZXW6YTBOI======')
+            \emdeevy\base32\Base32::decode('MZXW6YTBOI')
         );
     }
 
